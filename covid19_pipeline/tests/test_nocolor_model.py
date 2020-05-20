e556558e199eb8c88f10ae5d5a3f9ada56e64431
marsggbo <1435679023@qq.com>
2020-05-21 00:03:27 +0800

    test nocolor models

---
+++ covid19_pipeline/tests/test_nocolor_model.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('..')
+import torchline as tl
+from config.config import add_config
+import models
+import torch
+
+cfg = tl.config.get_cfg()
+cfg = add_config(cfg)
+cfg.merge_from_file('../config/config.yml')
+cfg.model.n_input_channels = 1
+
+x = torch.rand(1,1,32,64,64)
+
+model_names = {
+    'mc3_18': [],
+    'r3d_18': [],
+    'r2plus1d_18': [],
+    'densenet3d': [121,169,201,264],
+    'resnet3d': [10,18,34,50,101,152,200],
+    # 'wide_resnet3d': [50,101,152,200],
+    # 'resnext3d': [50,101,152,200],
+    # 'preact_resnet3d': [10,18,34,50,101,152,200],
+}
+
+for name in model_names:
+    depths = model_names[name]
+    if name == 'densenet3d':
+        cfg.model.model_depth = 121
+    if name in ['resnet3d',
+        'wide_resnet3d',
+        'resnext3d',
+        'preact_resnet3d']:
+        cfg.model.model_depth = 50
+    try:
+        cfg.model.name = name
+        net = tl.models.build_model(cfg)
+        y = net(x)
+        print(f"{name} pass")
+    except Exception as e:
+        cfg.model.name = name
+        net = tl.models.build_model(cfg)
+        y = net(x)
+        print(name, tl.utils.model_size(model),'MB')
+        print(name, 'not pass')
